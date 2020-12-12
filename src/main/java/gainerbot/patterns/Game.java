dc2332ba6e8611ac5c2edd9ceb6c266c9d3f2eb5
Flash3r01 <tim-johmann@web.de>
2020-12-12 16:10:44 +0100

    fixed escaping the *

--- src/main/java/gainerbot/patterns/Game.java
+++ src/main/java/gainerbot/patterns/Game.java
@@ -18,8 +18,8 @@ public class Game extends BasePattern {
                     "\nOy, retard! Take this piece of :soap: and wash your fucking mouth!\n" +
                     "And never mention this godforsaken word again on this server.\n" +
                     "I have been nice and censored your Message for you:\n\n> " +
-                    event.getMessage().getContentRaw().replace("game", "g*me")
-                            .replace("Game", "G*me")
+                    event.getMessage().getContentRaw().replace("game", "g\\*me")
+                            .replace("Game", "G\\*me")
                             .replace("\n", "\n> ");
 
             event.getMessage().delete().queue();
