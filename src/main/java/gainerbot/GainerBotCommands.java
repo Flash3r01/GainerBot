91ef28017bb62237ffa81c93c6fe1685a2513a1a
Flash3r01 <tim-johmann@web.de>
2021-01-14 17:10:51 +0100

    testing lp-cross

--- src/main/java/gainerbot/GainerBotCommands.java
+++ src/main/java/gainerbot/GainerBotCommands.java
@@ -29,7 +29,7 @@ public class GainerBotCommands {
         commands.add(new Pasta());
         commands.add(new OWOifier());
         commands.add(new Pattern());
-        //commands.add(new Sound());    //TODO fix the Sound on aarch64-processors
+        commands.add(new Sound());    //TODO fix the Sound on aarch64-processors
 
         //Extract all the commandNames.
         for(BaseCommand command : commands){
