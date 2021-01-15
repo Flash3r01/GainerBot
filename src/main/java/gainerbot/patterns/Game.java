54d5ce83d59de095f1a94225d9c1d0e440253573
Flash3r01 <tim-johmann@web.de>
2021-01-15 14:05:02 +0100

    fixed game pattern breaking links

--- src/main/java/gainerbot/patterns/Game.java
+++ src/main/java/gainerbot/patterns/Game.java
@@ -14,6 +14,10 @@ public class Game extends BasePattern {
     @Override
     public void run(@Nonnull MessageReceivedEvent event) {
         if(event.getMessage().getContentRaw().toLowerCase().contains("game")){
+            if(event.getMessage().getContentRaw().toLowerCase().contains("http")){
+                event.getChannel().sendMessage("Watch out! You probably used a rude word in a Link. Please be more careful next time!").queue();
+                return;
+            }
             String newMsg = event.getAuthor().getAsMention() +
                     "\nOy, retard! Take this piece of :soap: and wash your fucking mouth!\n" +
                     "And never mention this godforsaken word again on this server.\n" +
